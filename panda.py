30df6c5e3186e4126e2f9a342f6aa4a218b44992
Luca Fiorentino <fiorelux@gmail.com>
2017-02-11 12:27:41 +0100

    first version

--- panda.py
+++ panda.py
@@ -13,4 +13,17 @@ MyDataframe = pd.DataFrame({'INDEX':col1,
                               'THREE':np.array([3]*4, dtype='int32'),
                               'SET':pd.Categorical(["test", "train", "test", "train"]),
                               'CAT':col3})
-print(MyDataframe)
+# print(MyDataframe)
+# print(MyDataframe.head(2))
+# print(MyDataframe.tail(3))
+# print(MyDataframe.dtypes)
+# print(MyDataframe.columns)
+# print(MyDataframe['Date']) # column of a dataframe are also proprerty of an object so you can write print(MyDataframe.Data)
+
+# print(MyDataframe.ix[3,'SQUARED'])
+
+print(MyDataframe[(MyDataframe['CAT'] == 'cat1') | (MyDataframe['SET'] != 'test')])
+
+#MyDataframe['VALUES'] =[2,5,1]  this istruction doesn't work becouse there are only 3 elements
+
+MyDataframe.pop('THREE')
