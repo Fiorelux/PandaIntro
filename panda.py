66b57c96cc8643ad25b04af4230ae2ac177e8a2a
Luca Fiorentino <fiorelux@gmail.com>
2017-02-11 15:32:44 +0100

    first version

--- panda.py
+++ panda.py
@@ -62,33 +62,47 @@ MyDataframe['NUM_CAT'] = [1,1,2,3]
 # MyDataframe= MyDataframe.sort_values(['NUM_CAT', 'VALUES'], ascending=[True, False])
 # print(MyDataframe)
 
-# ## merging with other data frames
+# # ## merging with other data frames
 # MyDataframe2 = pd.DataFrame({'INDEX':[2,3,1,0],
 #                               'VALUE2':[11,22,33,44]})
-# print(MyDataframe2)
-#
-# # other options for how are 'inner', 'outer', 'right'
+# # print(MyDataframe2)
+# #
+# # # other options for how are 'inner', 'outer', 'right'
 # MyDataframe3 = pd.merge(MyDataframe, MyDataframe2,
-#                         how='left', on='INDEX')
-# print(MyDataframe3)
+#                          how='left', on='INDEX')
+# # print(MyDataframe3)
+#
+# Trainset = MyDataframe3[MyDataframe3['SET'] != 'TRAINSET']
+# print(Trainset)
+# # resetting rownumbers
+# Trainset = Trainset.reset_index()
+# print(Trainset)
+# # notice the index has now become a column
+# #  not to be confused with INDEX
+# #  so columns in pandas case sensitive
+# #  we can drop it
+# Trainset.pop("index")
+# print(Trainset)
+#
+# # setting a generic path
+# import os
+# outpath = os.path.abspath('./Results/export.csv')
+# print(outpath)
+# if not os.path.exists(os.path.abspath("./Results")):
+#     os.makedirs("./Results")
+# # exporting to csv file (without row numbers)
+# Trainset.to_csv(outpath, sep=';', index_label=False)
+#
+# Trainset_2 = pd.read_csv(os.path.abspath("./Results/export2.csv"), sep=";")
+# print(Trainset_2)
 
-Trainset = MyDataframe3[MyDataframe3['SET'] != 'TRAINSET']
-print(Trainset)
-# resetting rownumbers
-Trainset = Trainset.reset_index()
-print(Trainset)
-# notice the index has now become a column
-#  not to be confused with INDEX
-#  so columns in pandas case sensitive
-#  we can drop it
-Trainset.pop("index")
-print(Trainset)
+import matplotlib.pyplot as plt
+import matplotlib
 
-# setting a generic path
-import os
-outpath = os.path.abspath('./Results/export.csv')
-print(outpath)
-if not os.path.exists(os.path.abspath("./Results")):
-    os.makedirs("./Results")
-# exporting to csv file (without row numbers)
-Trainset.to_csv(outpath, index_label=False)
+#optional style
+matplotlib.style.use('ggplot')
+# draw 2000 random points and plot the sum
+df_rand = pd.Series(np.random.randn(2000))
+df_rand_sum = df_rand.cumsum()
+df_rand_sum.plot()
+plt.show()
