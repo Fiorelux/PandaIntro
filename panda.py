fc42d9f0d7797b13e4cd757eeeb18b94d2834c1a
Luca Fiorentino <fiorelux@gmail.com>
2017-02-11 12:00:54 +0100

    first version

--- panda.py
+++ panda.py
@@ -1,4 +1,15 @@
-from myModule import Animal
+import pandas as pd
+import numpy as np
 
-lion=Animal('George')
-lion.printName()
+col1 = [i for i in range(4)]
+col2 = [float(i**2) for i in range(4) ]
+col3 = ['cat1', 'cat2', 'cat3', 'cat1']
+
+
+MyDataframe = pd.DataFrame({'INDEX':col1,
+                              'DATE':pd.Timestamp('20130102'),
+                              'SQUARED':col2,
+                              'THREE':np.array([3]*4, dtype='int32'),
+                              'SET':pd.Categorical(["test", "train", "test", "train"]),
+                              'CAT':col3})
+print(MyDataframe)
