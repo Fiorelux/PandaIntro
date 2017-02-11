0f038ef491000bde278b2c521a05df0dcd0b897b
Luca Fiorentino <fiorelux@gmail.com>
2017-02-11 12:01:59 +0100

    first version

--- panda.py
+++ panda.py
@@ -6,6 +6,7 @@ col2 = [float(i**2) for i in range(4) ]
 col3 = ['cat1', 'cat2', 'cat3', 'cat1']
 
 
+
 MyDataframe = pd.DataFrame({'INDEX':col1,
                               'DATE':pd.Timestamp('20130102'),
                               'SQUARED':col2,
